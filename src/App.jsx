33a2b33da5395574631066da727e2716ba239d29
Rani <ranibhosale168@gmail.com>
2026-03-18 19:33:09 +0530

    added react  file props

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,60 @@
 import FirstComponent from './FirstComponent'
 import './App.css'
 
+import { userstate } from 'react';
 
-// function App() {
-//    <FirstComponent> </FirstComponent>
+
+function App() {
+   <FirstComponent> </FirstComponent>
+
+  return (
+    
+   <div>
+   <h1>hiiiiiiiii</h1>
+   <p>hello</p> 
+  <FirstComponent> </FirstComponent>
+   </div>  
+      
+  )
+}
+
+
+
+// function App(){
+//   let isLoggedIn = true;
+//   let isVisiable = false;
+
+//     let message;
+//   if (isLoggedIn) {
+//     message = <p>Welcome Back</p>;
+//   } else {
+//     message = <p>Please Login</p>;
+//   }
 
 //   return (
-    
-//    <div>
-//    <h1>hiiiiiiiii</h1>
-//    <p>hello</p> 
-//   <FirstComponent> </FirstComponent>
-//    </div>  
-
-   
-
-      
+//     <div>
+//       {isLoggedIn ? <p> Welcome Back</p> : <p>Login in</p>}
+//         {isVisiable && <div>thiss is div </div>}
+//         <hr />
+//         <TextCom text="My name is Rani" city="Pune" age={21}></TextCom>
+//         <TextCom text="I live in pune."></TextCom>
+//         <TextCom text="I love to sketching"></TextCom>
+//     </div>
 //   )
 // }
 
-
-
-function App(){
-  let isLoggedIn = true;
-  let isVisiable = false;
-
-    let message;
-  if (isLoggedIn) {
-    message = <p>Welcome Back</p>;
-  } else {
-    message = <p>Please Login</p>;
-  }
-
-  return (
-    <div>
-      {isLoggedIn ? <p> Welcome Back</p> : <p>Login in</p>}
-        {isVisiable && <div>thiss is div </div>}
-        <hr />
-        <TextCom text="My name is Rani" city="Pune" age={21}></TextCom>
-        <TextCom text="I live in pune."></TextCom>
-        <TextCom text="I love to sketching"></TextCom>
-    </div>
-  )
-}
-
-function TextCom(props){
-  console.log("somthing", props)
-  return (
-    <div>
-      <p>{props.text}</p>
-      <p>{props.city}</p>
-      <p>{props.age}</p>
+// function TextCom(props){
+//   console.log("somthing", props)
+//   return (
+//     <div>
+//       <p>{props.text}</p>
+//       <p>{props.city}</p>
+//       <p>{props.age}</p>
+//       <FirstComponent> </FirstComponent>
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
 export default App
 
--- src/FirstComponent.jsx
+++ src/FirstComponent.jsx
@@ -1,6 +1,74 @@
+import { useState } from 'react';
 function FirstComponent(){
-    return <h1>Rani</h1>
+    let [num, setNum] = useState(0);
+    function increase(){
+        setNum(num + 1);
+    }
+
+    function decrese(){
+        setNum(num - 1);
+    }
+
+    function reseat(){
+        setNum(0);
+    }
+
+    return (
+        <div>
+            <h1>num is {num}</h1>
+            <button onClick={increase}>Increase</button>
+            <button onClick={decrese}>decerse</button>
+            <button onClick={reseat}>resest</button>
+
+        </div>
+    )
+// let info = [
+//     {
+//       "id": 1,
+//       "todo": "Do something nice for someone you care about",
+//       "completed": false,
+//       "userId": 152
+//     },
+//     {
+//       "id": 2,
+//       "todo": "Memorize a poem",
+//       "completed": true,
+//       "userId": 13
+//     },
+//     {
+//       "id": 3,
+//       "todo": "Watch a classic movie",
+//       "completed": true,
+//       "userId": 68
+//     },
+//     {
+//       "id": 4,
+//       "todo": "Watch a documentary",
+//       "completed": false,
+//       "userId": 84
+//     }]
+
+    // let newarray = info.map((s)=>{
+    //     return<div key={s.id}>
+    //     <p>{s.todo}</p>
+    //     <p>Status: {s.completed ? "Done" : "Pending"}</p>
+    //   </div>
+    // });
+    // return (
+    //     <div>
+    //         <h1>React js day 03</h1>
+    //         {newarray}
+    //     </div>
+    // )
 }
 
 
+
+
+
+
+// when we use arrray or list by map then use unque key
+  // unidirsection flow of data in react
+  //state :
+//it is a value ,which can change // when it change then update the coponent automatice //  state is rerender 
 export default FirstComponent;
